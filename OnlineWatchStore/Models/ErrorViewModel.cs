d879127004f46d060fbe265b39c6cb338a26f805
Ahmad276n <ak5255744@gmail.com>
2023-11-14 10:50:34 -0800

    Initial Created

---
+++ OnlineWatchStore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OnlineWatchStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
